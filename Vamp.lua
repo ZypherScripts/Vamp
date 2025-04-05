ba910833aae20186b2be1afaccd8217fd8c2d43e
ZypherScripts <aminelafemmedu91@gmail.com>
2025-04-05 09:19:37 -0700

    Update Vamp.lua

--- Vamp.lua
+++ Vamp.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZypherScripts/Vamp/refs/heads/main/Gamelist.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZypherScripts/Vamp/refs/heads/main/Gamelist.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
