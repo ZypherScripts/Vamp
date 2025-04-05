dd917118e193bf0852d0df9bd0715fea2643b864
ZypherScripts <aminelafemmedu91@gmail.com>
2025-04-05 10:04:24 -0700

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,6 +1,8 @@
 local Games = {
-[3623096087] = "https://raw.githubusercontent.com/ZypherScripts/Vamp/refs/heads/main/Muscle%20Legends.lua", -- Muscle Legends
-[9872472334] = "https://raw.githubusercontent.com/ZypherScripts/Vamp/refs/heads/main/Evade.lua", -- Evade
+[3623096087] = "https://raw.githubusercontent.com/ZypherScripts/Script-Games/refs/heads/main/Muscle%20Legends.lua", -- Muscle Legends
+[9872472334] = "https://raw.githubusercontent.com/ZypherScripts/Script-Games/refs/heads/main/Evade.lua", -- Evade
+[13772394625] = "https://raw.githubusercontent.com/ZypherScripts/Script-Games/refs/heads/main/Blade%20Ball.lua", -- Blade Ball
+[6516141723] = "https://raw.githubusercontent.com/ZypherScripts/Script-Games/refs/heads/main/Doors.lua", -- Doors
 }
 
 return Games
